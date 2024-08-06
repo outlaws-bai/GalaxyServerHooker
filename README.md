48b5b7e3e4be315a19ba529f561ae577c27f8373
outlaws <outlaws@88.com>
2024-08-06 17:57:54 +0800

    rm fast

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # PyGRpcServer
-**提供一个[Galaxy](https://github.com/outlaws-bai/Galaxy)中需要的Rpc实现，使用python3.11**
+提供一个[Galaxy](https://github.com/outlaws-bai/Galaxy)中需要的Rpc实现
+
+> python3.9+
+
+## install
+
+```bash
+pip install -r requirements.txt
+python manager.py
+```
--- manager.py
+++ manager.py
@@ -5,15 +5,35 @@
 import json
 import time
 import grpc
+import base64
 from concurrent import futures
 from models import *
-from server_fast import aes_decrypt, aes_encrypt
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
 
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 PORT = 8443
 
 get_encrypt_text = lambda x: json.loads(x)["data"]
 to_encrypt_body = lambda x: json.dumps({"data": x.decode()}).encode()
+
+KEY = b"32byteslongsecretkeyforaes256!aa"
+IV = b"16byteslongiv456"
+
+
+# AES加密函数
+def aes_encrypt(data: bytes) -> bytes:
+    cipher = AES.new(KEY, AES.MODE_CBC, IV)
+    ct_bytes = cipher.encrypt(pad(data, AES.block_size))
+    return base64.b64encode(ct_bytes)
+
+
+# AES解密函数
+def aes_decrypt(data: bytes) -> bytes:
+    data1 = base64.b64decode(data)
+    cipher = AES.new(KEY, AES.MODE_CBC, IV)
+    pt = unpad(cipher.decrypt(data1), AES.block_size)
+    return pt
 
 
 class GRpcServicer(HttpHookService):
